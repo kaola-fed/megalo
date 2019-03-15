c3e157e545e8e3d90feed229fa85c0c55aca3797
elcarim5efil <elcarim5efil@gmail.com>
2019-03-15 20:26:24 +0800

    feat: basic support for toutiao #55

--- src/platforms/mp/util/presets/index.js
+++ src/platforms/mp/util/presets/index.js
@@ -1,9 +1,11 @@
 import wechat from './preset-wechat'
 import alipay from './preset-alipay'
 import swan from './preset-swan'
+import toutiao from './preset-toutiao'
 
 export default {
   wechat,
   alipay,
-  swan
+  swan,
+  toutiao
 }
