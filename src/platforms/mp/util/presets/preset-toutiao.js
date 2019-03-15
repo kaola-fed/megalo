81300553e36ca512497edcd515ebe2f6149d0e7a
elcarim5efil <elcarim5efil@gmail.com>
2019-03-15 20:26:24 +0800

    feat: basic support for toutiao #55

---
+++ src/platforms/mp/util/presets/preset-toutiao.js
@@ -0,0 +1,64 @@
+import { basePrest } from './preset-base'
+import {
+  createFindEventTypeFn,
+  mergePreset
+} from './helper'
+
+const prefix = `tt:`
+
+export const eventTypeMap = {
+  tap: ['tap', 'click'],
+  touchstart: ['touchstart'],
+  touchmove: ['touchmove'],
+  touchcancel: ['touchcancel'],
+  touchend: ['touchend'],
+  longtap: ['longtap'],
+  input: ['input'],
+  blur: ['change', 'blur'],
+  submit: ['submit'],
+  focus: ['focus'],
+  scrolltoupper: ['scrolltoupper'],
+  scrolltolower: ['scrolltolower'],
+  scroll: ['scroll']
+}
+
+export const findEventType = createFindEventTypeFn(eventTypeMap)
+
+export default mergePreset(basePrest, {
+  prefix,
+  ext: `ttml`,
+  directives: {
+    if: `${prefix}if`,
+    elseif: `${prefix}elif`,
+    else: `${prefix}else`,
+    for: `${prefix}for`,
+    forItem: `${prefix}for-item`,
+    forIndex: `${prefix}for-index`,
+    forKey: `${prefix}key`,
+    on: `bind`,
+    onStop: `catch`,
+    capture: `capture`
+  },
+  eventTypeMap,
+  findEventType,
+  genBind (event, type, tag) {
+    const { modifiers = {}} = event
+    const isCapture = /!/.test(type)
+    const realType = type.replace(/^[~|!]/, '')
+    const { stop } = modifiers
+    let mpType = realType
+    let binder = stop ? 'catch' : 'bind'
+    binder = isCapture ? `capture-${binder}` : binder
+
+    if (binder !== 'bind') {
+      binder = `${binder}:`
+    }
+
+    if (type === 'change' && (tag === 'input' || tag === 'textarea')) {
+      mpType = 'blur'
+    } else {
+      mpType = mpType === 'click' ? 'tap' : mpType
+    }
+    return `${binder}${mpType}`
+  }
+})
