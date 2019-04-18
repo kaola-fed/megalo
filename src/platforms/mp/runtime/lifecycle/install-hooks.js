48e4e3b5744956562953bb59ca5b9cbda47db508
elcarim5efil <elcarim5efil@gmail.com>
2019-04-18 11:00:42 +0800

    fix: access hooks from options in typescript #242

--- src/platforms/mp/runtime/lifecycle/install-hooks.js
+++ src/platforms/mp/runtime/lifecycle/install-hooks.js
@@ -2,7 +2,11 @@ import { callHook } from './call-hook'
 
 export function installHooks(pageOptions, vueOptions, hooks) {
   hooks.forEach(hook => {
-    if (vueOptions[hook]) {
+    if (
+      vueOptions[hook] ||
+      // hooks definition is store in options in typescript
+      (vueOptions.options && vueOptions.options[hook])
+    ) {
       pageOptions[hook] = function hookFn(options) {
         return callHook(this.rootVM, hook, options)
       }
